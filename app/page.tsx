34e4d538fed83b484a312401f5199151eca97a99
Jetross <jetrossneri07@gmail.com>
2026-05-03 23:36:53 +0800

    feat: add typography component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { ModeToggle } from "@/components/mode-toggle";
+import { TypographyH1 } from "@/components/ui/typography";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between">
+    <div className="flex min-h-screen flex-row items-center justify-between">
       <ModeToggle />
+      <TypographyH1>Welcome to E-Wise</TypographyH1>
     </div>
   );
 }
---
+++ components/ui/typography.tsx
@@ -0,0 +1,97 @@
+import React from "react";
+
+export function TypographyH1({ children }: { children: React.ReactNode }) {
+  return (
+    <h1 className="scroll-m-20 text-center text-3xl md:text-5xl font-extrabold tracking-tight text-balance">
+      {children}
+    </h1>
+  );
+}
+
+export function TypographyH2({ children }: { children: React.ReactNode }) {
+  return (
+    <h2 className="scroll-m-20 border-b pb-2 text-2xl md:text-3xl font-semibold tracking-tight first:mt-0">
+      {children}
+    </h2>
+  );
+}
+
+export function TypographyH3({ children }: { children: React.ReactNode }) {
+  return (
+    <h3 className="scroll-m-20 text-sm sm:text-xl md:text-2xl font-semibold tracking-tight">
+      {children}
+    </h3>
+  );
+}
+
+export function TypographyH4({ children }: { children: React.ReactNode }) {
+  return (
+    <h4 className="scroll-m-20 text-base md:text-xl font-semibold tracking-tight">
+      {children}
+    </h4>
+  );
+}
+
+export function TypographyP({ children }: { children: React.ReactNode }) {
+  return (
+    <p className="max-w-md text-sm md:text-base leading-relaxed [&:not(:first-child)]:mt-2">
+      {children}
+    </p>
+  );
+}
+
+export function TypographyBlockquote({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <blockquote className="mt-6 border-l-2 pl-6 italic">{children}</blockquote>
+  );
+}
+
+export function TypographyInlineCode({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
+      {children}
+    </code>
+  );
+}
+
+export function TypographyLead({ children }: { children: React.ReactNode }) {
+  return <p className="text-xl text-muted-foreground">{children}</p>;
+}
+
+export function TypographyLarge({ children }: { children: React.ReactNode }) {
+  return <div className="text-lg font-semibold">{children}</div>;
+}
+
+export function TypographySmall({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <small
+      className={`text-sm leading-none font-medium${className ? ` ${className}` : ""}`}
+    >
+      {children}
+    </small>
+  );
+}
+
+export function TypographyNav({ children }: { children: React.ReactNode }) {
+  return (
+    <small className="text-sm leading-none font-semibold">{children}</small>
+  );
+}
+
+export function TypographyMuted({ children }: { children: React.ReactNode }) {
+  return <p className="text-sm text-muted-foreground">{children}</p>;
+}
