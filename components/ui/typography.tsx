73c854f68747e002156b28602087647acb3435ed
Jetross <jetrossneri07@gmail.com>
2026-05-08 22:04:38 +0800

    feat: add hero section component

--- components/ui/typography.tsx
+++ components/ui/typography.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function TypographyH1({ children }: { children: React.ReactNode }) {
   return (
-    <h1 className="scroll-m-20 text-center text-3xl md:text-5xl font-extrabold tracking-tight text-balance">
+    <h1 className="scroll-m-20 text-center text-4xl md:text-7xl font-extrabold tracking-tight text-balance">
       {children}
     </h1>
   );
